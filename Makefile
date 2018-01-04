6ef1243e46f6c1555c2c64fd8c22f5a76d3b9724
Alexander Luberg <alex@luberg.me>
2018-01-04 09:22:44 -0800

    rewrote in obj-c

--- Makefile
+++ Makefile
@@ -1,4 +1,15 @@
-all: main
+BUILD_DIR = build
 
-main: main.c ; clang -framework CoreFoundation -framework IOKit main.c -o unfucker
+.PHONY: all clean main settings
+
+all: clean main settings
+
+settings: settings/uvc/UVCCameraControl.h settings/uvc/UVCCameraControl.m settings/main.m
+	clang -framework Foundation -framework IOKit settings/uvc/UVCCameraControl.m settings/main.m -o $(BUILD_DIR)/settings
+
+main: daemon/main.c
+	clang -framework CoreFoundation -framework IOKit daemon/main.c -o $(BUILD_DIR)/daemon
+
+clean:
+	rm -f $(BUILD_DIR)/*
 
---
+++ settings/main.m
@@ -0,0 +1,27 @@
+#import <Foundation/Foundation.h>
+#include <xpc/xpc.h>
+#include <unistd.h>
+#include "uvc/UVCCameraControl.h"
+
+#define VID 0x46d
+#define PID 0x85e
+
+int main(int argc, const char *argv[])
+{
+	@autoreleasepool {
+		UVCCameraControl *cameraControl = [[UVCCameraControl alloc] initWithVendorID:VID productID:PID interfaceNum:0x00];
+
+		[cameraControl setAutoFocus:NO];
+		[cameraControl setAutoWhiteBalance:NO];
+
+		[cameraControl setAbsoluteFocus:0];
+		[cameraControl setBrightness:0.5];
+		[cameraControl setSharpness:0.5];
+		[cameraControl setContrast:0.5];
+		[cameraControl setSaturation:0.5];
+		[cameraControl setWhiteBalance:0.4];
+
+		[cameraControl release];
+	}
+	return 0;
+}
